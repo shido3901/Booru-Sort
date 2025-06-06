3aac6b7072186656285e18721a509b5184aaa0ef
shido3901 <kracing804@gmail.com>
2025-06-05 20:25:49 -0700

    lacking fundmentals
    Old code was so screwed up. had AI help me clean up the code, and make proper classes. Going to clean it up even more to get rid of the parameter passing.

--- main_window.py
+++ main_window.py
@@ -1,44 +1,37 @@
 import sys
-from PyQt5.QtWidgets import (QApplication, QMainWindow, QLabel, QWidget,
-                            QWidget, QVBoxLayout, QHBoxLayout, QGridLayout, QPushButton)
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QLabel, QWidget, QLineEdit, QSizePolicy,
+                             QVBoxLayout, QHBoxLayout, QGridLayout, QPushButton)
 from PyQt5.QtGui import QFont
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QEvent
 
 from tag import add_tag_to_grid
-from view_elements import  add_image_to_area, refresh_image_area, set_image_size
-
-image_cell_width = 250
-image_cell_height = 420
-image_size = 2
-
+from view_elements import ImageManager  
 
 class MainWindow(QMainWindow):
-    global image_cell_width, image_cell_height
-
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Booru Sort Lite")
-        self.setGeometry(700,300,1920,1080)
-        self.setStyleSheet("background-color: black;") 
-        self.initUI()
-        self.num_cols = self.image_area.width() // image_cell_width
-        self.num_rows = self.image_area.height() // image_cell_height
+        self.setGeometry(700, 300, 1920, 1200)
+        self.setStyleSheet("background-color: black;")
 
+        self.image_manager = ImageManager()
+
+        self.initUI()
+
+        self.num_cols = self.image_area.width() // self.image_manager.image_cell_width
+        self.num_rows = self.image_area.height() // self.image_manager.image_cell_height
+
+        self.image_manager.refresh_image_area(self.image_area_layout, self.num_cols, self.num_rows)
 
     def initUI(self):
         main_window = QWidget()
         self.setCentralWidget(main_window)
 
         self.tag_list_panel = QWidget(main_window)
-        self.tag_list_panel.setMinimumWidth(270) 
-        self.tag_list_panel.setMaximumWidth(450)
+        self.tag_list_panel.setMinimumWidth(320)
+        self.tag_list_panel.setMaximumWidth(520)
         self.tag_list_panel.setStyleSheet("background-color: #454545;")
-
-
-        
-
         self.tag_list_layout = QGridLayout()
-    
         self.tag_list_panel.setLayout(self.tag_list_layout)
 
         self.image_area = QWidget(main_window)
@@ -48,115 +41,128 @@ class MainWindow(QMainWindow):
 
         self.search_bar = QWidget(main_window)
         self.search_bar.setMinimumWidth(200)
-        self.search_bar.setMinimumHeight(50)
-        self.search_bar.setMaximumHeight(50)
+        self.search_bar.setMinimumHeight(80)
+        self.search_bar.setMaximumHeight(80)
         self.search_bar.setStyleSheet("background-color: #454545;")
-        
+
+        text_box = QLineEdit(self.search_bar)
+        text_box.setPlaceholderText("Search Example: black_cat")
+        text_box.setFixedHeight(70)
+        text_box.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        text_box.setStyleSheet("""
+            QLineEdit {
+                background-color: black;
+                color: white;
+                border: 3px;
+                padding: 5px;
+            }
+        """)
+
         self.options_bar = QWidget(main_window)
         self.options_bar.setMinimumWidth(180)
         self.options_bar.setMinimumHeight(50)
         self.options_bar.setMaximumHeight(50)
         self.options_bar.setStyleSheet("background-color: #454545;")
-
         options_layout = QVBoxLayout(self.options_bar)
         options_layout.setContentsMargins(0, 0, 0, 0)
+
+        search_layout = QHBoxLayout(self.search_bar)
+        search_layout.setContentsMargins(5, 0, 0, 0)
+        search_layout.setSpacing(5)
+        search_layout.addWidget(text_box)
 
         tag_button = QPushButton("change size", self.options_bar)
         tag_button.setStyleSheet("color: white; background-color: #606060; border: none;")
         tag_button.clicked.connect(self.change_size)
-        options_layout.addWidget(tag_button, alignment=Qt.AlignCenter)
         tag_button.setFixedSize(120, 40)
+        options_layout.addWidget(tag_button, alignment=Qt.AlignCenter)
 
-        
-    
         self.bottom_bar = QWidget(main_window)
-        self.bottom_bar.setMinimumWidth(100)
         self.bottom_bar.setMinimumHeight(50)
         self.bottom_bar.setMaximumHeight(50)
         self.bottom_bar.setStyleSheet("background-color: #454545;")
+        self.bottom_bar.setContentsMargins(0, 0, 0, 0)
+
+        bottom_bar_layout = QHBoxLayout()
+        bottom_bar_layout.setContentsMargins(10, 0, 10, 0)
+        bottom_bar_layout.setSpacing(20)
+        self.bottom_bar.setLayout(bottom_bar_layout)
+
+        previous_page_click = QPushButton("Previous Page", self.bottom_bar)
+        previous_page_click.setStyleSheet("color: white; background-color: #606060; border: none;")
+        previous_page_click.setFixedSize(120, 40)
+        previous_page_click.clicked.connect(self.previous_page_click)
+
+        self.page_count = QLabel("", self.bottom_bar)
+        self.page_count.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
+        self.page_count.setAlignment(Qt.AlignCenter)
+
+        next_page_click = QPushButton("Next Page", self.bottom_bar)
+        next_page_click.setStyleSheet("color: white; background-color: #606060; border: none;")
+        next_page_click.setFixedSize(120, 40)
+        next_page_click.clicked.connect(self.next_page_click)
+
+        bottom_bar_layout.addWidget(previous_page_click, alignment=Qt.AlignLeft)
+        bottom_bar_layout.addWidget(self.page_count, alignment=Qt.AlignCenter)
+        bottom_bar_layout.addWidget(next_page_click, alignment=Qt.AlignRight)
 
         self.image_area_layout = QGridLayout()
         self.image_area.setLayout(self.image_area_layout)
 
-      
         grid = QGridLayout()
-
         grid.addWidget(self.tag_list_panel, 0, 0, 5, 1)
-   
         grid.addWidget(self.image_area, 1, 1, 2, 2)
         grid.addWidget(self.search_bar, 0, 1)
         grid.addWidget(self.options_bar, 0, 2)
         grid.addWidget(self.bottom_bar, 3, 1, 2, 2)
 
-        grid.setColumnStretch(0, 0)  
-        grid.setColumnStretch(1, 1)  
-
-        grid.setRowStretch(0, 1)  
-        grid.setRowStretch(1, 1)    
-        grid.setSpacing(5)   
+        grid.setColumnStretch(0, 0)
+        grid.setColumnStretch(1, 1)
+        grid.setRowStretch(0, 1)
+        grid.setRowStretch(1, 1)
+        grid.setSpacing(5)
         grid.setContentsMargins(6, 6, 6, 6)
-       
+
         main_window.setLayout(grid)
 
+        self.image_area.installEventFilter(self)
         add_tag_to_grid(self.tag_list_layout)
-        add_image_to_area(self.image_area_layout)
-
-       
 
     def resizeEvent(self, event):
-        global latest_num_cols, latest_num_rows
-        latest_num_cols = self.image_area.width() // image_cell_width
-        latest_num_rows = self.image_area.height() // image_cell_height
+        latest_num_cols = self.image_area.width() // self.image_manager.image_cell_width
+        latest_num_rows = self.image_area.height() // self.image_manager.image_cell_height
 
         if latest_num_cols != self.num_cols or latest_num_rows != self.num_rows:
             self.num_cols = latest_num_cols
             self.num_rows = latest_num_rows
-     
-            
-            refresh_image_area(self.image_area_layout, latest_num_cols, latest_num_rows)
+            self.image_manager.refresh_image_area(self.image_area_layout, self.num_cols, self.num_rows)
 
         event.accept()
 
     def change_size(self):
-        global image_size,  image_cell_height, image_cell_width, latest_num_cols, latest_num_rows
+        latest_num_cols = self.image_area.width() // self.image_manager.image_cell_width
+        latest_num_rows = self.image_area.height() // self.image_manager.image_cell_height
+        self.image_manager.set_image_size(self.image_area_layout, latest_num_cols, latest_num_rows)
 
-        if image_size == 1:
-            image_cell_width = 250
-            image_cell_height = 360
-           
-            image_size = 2
+    def next_page_click(self):
+        latest_num_cols = self.image_area.width() // self.image_manager.image_cell_width
+        latest_num_rows = self.image_area.height() // self.image_manager.image_cell_height
+        self.image_manager.next_page(self.image_area_layout, latest_num_cols, latest_num_rows)
 
-        elif image_size == 2:
-            image_cell_width = 360
-            image_cell_height = 430
-            image_size = 3
-            
+    def previous_page_click(self):
+        latest_num_cols = self.image_area.width() // self.image_manager.image_cell_width
+        latest_num_rows = self.image_area.height() // self.image_manager.image_cell_height
+        self.image_manager.previous_page(self.image_area_layout, latest_num_cols, latest_num_rows)
 
-        elif image_size == 3:
-            image_cell_width = 130
-            image_cell_height = 180
-            image_size = 1
-            
-
-        latest_num_cols = self.image_area.width() // image_cell_width
-        latest_num_rows = self.image_area.height() // image_cell_height
-
-        if latest_num_cols != self.num_cols or latest_num_rows != self.num_rows:
-            self.num_cols = latest_num_cols
-            self.num_rows = latest_num_rows
-
-
-
-        set_image_size(self.image_area_layout, self.num_cols, self.num_rows)
-
-
-  
-
-
-
-
-
-
+    def eventFilter(self, source, event):
+        if source == self.image_area and event.type() == QEvent.Wheel:
+            delta = event.angleDelta().y()
+            if delta > 0:
+                self.previous_page_click()
+            else:
+                self.next_page_click()
+            return True
+        return super().eventFilter(source, event)
 
 def main():
     app = QApplication(sys.argv)
--- view_elements.py
+++ view_elements.py
@@ -1,96 +1,101 @@
+# view_elements.py
 from PyQt5.QtWidgets import QLabel, QSizePolicy
 from PyQt5.QtCore import Qt
 
-max_column = 6
-row = 2
-entity_count = 26
-total_on_screen = 32
-image_size = 2
+class ImageManager:
+    def __init__(self):
+        self.image_size = 2
+        self.image_cell_width = 250
+        self.image_cell_height = 420
 
-def set_image_size(image_area_layout, latest_num_cols, latest_num_rows):
-     
-        global max_column, total_on_screen, image_size
+        self.max_column = 6
+        self.row = 2
+        self.entity_count = 2345
+        self.starting_count = 0
+        self.total_on_screen = 0
+        self.page_count = 0
+        self.current_page = 0
+        self.amount_on_current_page = 0
+        self.temporary_page_count_save = 0
+        self.fit_all = True
 
-        max_column = latest_num_cols
+    def update_cell_size(self):
+        size_map = {
+            1: (130, 180),
+            2: (250, 360),
+            3: (360, 430)
+        }
+        self.image_cell_width, self.image_cell_height = size_map[self.image_size]
+
+    def set_image_size(self, image_area_layout, latest_num_cols, latest_num_rows):
+        self.image_size = {1: 2, 2: 3, 3: 1}[self.image_size]
+        self.update_cell_size()
+        self.refresh_image_area(image_area_layout, latest_num_cols, latest_num_rows)
+
+
+    def refresh_image_area(self, image_area_layout, latest_num_cols, latest_num_rows):
+        self.max_column = latest_num_cols
         max_row = latest_num_rows
+        self.total_on_screen = self.max_column * max_row
 
-        if image_size == 1:
-            #print('2')
-            image_size = 2
-
-        elif image_size == 2:
-            image_size = 3
-            #print('3')
-
-        elif image_size == 3:
-            image_size = 1
-            #print('1')
-
-        
-        
-        refresh_image_area(image_area_layout, latest_num_cols, latest_num_rows)
-
-def refresh_image_area(image_area_layout, latest_num_cols, latest_num_rows):
-    global max_column, total_on_screen, image_size
-
-    max_column = latest_num_cols
-    max_row = latest_num_rows
-
-    while image_area_layout.count():
+        # Clear layout
+        while image_area_layout.count():
             child = image_area_layout.takeAt(0)
             if child.widget():
                 child.widget().deleteLater()
 
-    total_on_screen = max_row * max_column
-    
+        if self.total_on_screen >= self.entity_count and self.fit_all:
+            self.temporary_page_count_save = self.starting_count
+            self.fit_all = False
+            self.starting_count = 0
+        elif not self.fit_all:
+            self.starting_count = self.temporary_page_count_save
+            self.fit_all = True
 
-    add_image_to_area(image_area_layout)
+        self._add_image_to_area(image_area_layout)
 
+    def _add_image_to_area(self, layout):
+        row = 0
+        col = 0
+        self.amount_on_current_page = 0
 
-def add_image_to_area(elements_area):
-    global row, current_column, entity_count, total_on_screen, max_column
-    row = 0
-    current_column = 0
+        for i in range(self.starting_count, self.starting_count + self.total_on_screen):
+            if i < self.entity_count:
+                label = QLabel(f"{i + 1}")
+                label.setStyleSheet("color: white; font-size: 50px; background-color: black;")
+                self.amount_on_current_page += 1
+            else:
+                label = QLabel()
+                label.setStyleSheet("background-color: #454545;")
 
-    for i in range(total_on_screen):
- 
-        
-        if i <= entity_count - 1:
-            add_element_to_area = QLabel(f"{i + 1} ") 
-            add_element_to_area.setStyleSheet("color: white; font-size: 50px;"
-                                            "background-color: black;")
+            size_map = {
+                1: (180, 100),
+                2: (360, 200),
+                3: (500, 275)
+            }
 
-        else:
-            add_element_to_area = QLabel()
-            add_element_to_area.setStyleSheet("background-color: #454545;")
-                
-        
+            height, width = size_map[self.image_size]
+            label.setMaximumHeight(height)
+            label.setMaximumWidth(width)
+            label.setAlignment(Qt.AlignCenter)
+            label.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Maximum)
 
-        if image_size == 1:
-            add_element_to_area.setMaximumHeight(180)
-            add_element_to_area.setMaximumWidth(100)
+            layout.addWidget(label, row, col)
+            col += 1
+            if col == self.max_column:
+                col = 0
+                row += 1
 
-        elif image_size == 2:
-            add_element_to_area.setMaximumHeight(360)
-            add_element_to_area.setMaximumWidth(200)
+        self.current_page = (self.starting_count // self.total_on_screen) + 1 if self.total_on_screen > 0 else 1
+        self.page_count = (self.entity_count + self.total_on_screen - 1) // self.total_on_screen if self.total_on_screen else 1
 
-        elif image_size == 3:
-            add_element_to_area.setMaximumHeight(500)
-            add_element_to_area.setMaximumWidth(275)
+    def next_page(self, layout, latest_num_cols, latest_num_rows):
+        if self.starting_count < self.entity_count - self.total_on_screen:
+            self.starting_count += self.total_on_screen
+            self.refresh_image_area(layout, latest_num_cols, latest_num_rows)
 
-
-
-
-        add_element_to_area.setAlignment(Qt.AlignCenter)
-        add_element_to_area.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Maximum)
-        elements_area.addWidget(add_element_to_area, row, current_column)
-
-        current_column += 1
-        
-        if current_column == max_column:
-            row += 1
-            current_column = 0
-
-
-
-
+    def previous_page(self, layout, latest_num_cols, latest_num_rows):
+        self.starting_count -= self.total_on_screen
+        if self.starting_count < 0:
+            self.starting_count = 0
+        self.refresh_image_area(layout, latest_num_cols, latest_num_rows)

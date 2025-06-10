fe75514a8c9ded7308f8b278d5c641b7346f7a4f
shido3901 <kracing804@gmail.com>
2025-06-09 23:40:06 -0700

    Delete side_panel.py

--- side_panel.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-
-def side_panel(parent):
-    side_panel = tk.Frame(parent, bg="#171717", width=320, height=40, borderwidth = 2, relief = tk.RIDGE)
-    side_panel.grid(row=1, column=0, sticky="nsew")
-
-    return side_panel
